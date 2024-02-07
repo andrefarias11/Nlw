301e121f7d650700017c501a0167869e8a7796cf
andrefarias <andre@DESKTOP-S42A2E8>
2024-02-06 21:58:39 -0300

    New branch

---
+++ src/RocketseatAuction.API/Controllers/AuctionController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RocketseatAuction.API.UseCases.Actions.GetCurrent;
+
+namespace RocketseatAuction.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuctionController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetCurrentAuction()
+    {
+        var useCase = new GetCurrentAuctionsUseCase();
+
+        var result = useCase.Execute();
+
+        return Ok(result);
+    }
+}
+
---
+++ src/RocketseatAuction.API/UseCases/Actions/GetCurrent/GetCurrentAuctionsUseCase.cs
@@ -0,0 +1,19 @@
+﻿using RocketseatAuction.API.Entities;
+
+namespace RocketseatAuction.API.UseCases.Actions.GetCurrent;
+
+public class GetCurrentAuctionsUseCase
+{
+    public Auction Execute()
+    {
+        return new Auction
+        {
+            Id = 1,
+            Name = "Test",
+            Starts = DateTime.Now,
+            Ends = DateTime.Now,
+        };
+
+    }
+}
+
